1512888e9cd04b755e551e5c0e4ae76f8629d93f
Pedro Alexander Lacan <pedroalexanderlacan@gmail.com>
2026-06-25 14:22:07 -0600

    Adding the structure of the first service to (Java)

---
+++ service-a-library/src/main/java/com/biblioteca/servicea/ServiceALibraryApplication.java
@@ -0,0 +1,13 @@
+package com.biblioteca.servicea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceALibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceALibraryApplication.class, args);
+	}
+
+}
